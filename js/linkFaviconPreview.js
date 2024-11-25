66c6d1321fb62577c3bebc610a4d7041b4eca040
he_coyc <115868356+he-coyc@users.noreply.github.com>
2024-11-25 08:38:40 +0700

    Delete js directory

--- js/linkFaviconPreview.js
+++
@@ -1,7 +0,0 @@
-
-const links = document.querySelectorAll('.link');
-links.forEach(element => {
-    let url = 'https://' + element.getAttribute('href').replace('https://', '').split('/')[0] + '/favicon.ico'; 
-    element.style.backgroundImage = `url('` + url + `')`;
-});
-
